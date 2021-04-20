1ecc8998915d49e6c8a8718a30d9ba8b43b86250
Alex Thompson <ahthomps1@gmail.com>
2021-04-20 14:04:52 -0600

    Connect NewsView component with App and Stroy component

---
+++ src/components/NewsView/NewsView.js
@@ -0,0 +1,14 @@
+import './newsView.css';
+import React from 'react';
+import Story from '../Story/Story';
+
+const NewsView = () => {
+
+  return (
+    <section>
+      <Story />
+    </section>
+  )
+}
+
+export default NewsView;
