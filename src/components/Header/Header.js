f5e781fc39f99d72bb575c330e19d0a973c904e3
Alex Thompson <ahthomps1@gmail.com>
2021-04-20 15:58:59 -0600

    Add basic styling and functionality to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,12 @@ import React from 'react';
 const Header = () => {
 
   return (
-    <header>
-      <h1>Current Climate</h1>
-      <p>sourcing important climate articles in an easy straightforward way.</p>
-      <img src='/images/logo2.svg' alt='website logo' />
+    <header className='nav-box'>
+      <div className='nav-text-box'>
+        <h1 className='nav-title'>Current Climate</h1>
+        <p className='nav-overview'>sourcing important climate articles in an easy straightforward way.</p>
+      </div>
+      <img className='nav-logo' src='/images/logo2.svg' alt='website logo' />
     </header>
   )
 }
