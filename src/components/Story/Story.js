ac6f86f75c7a6cbbe828806fbc96d93becf9e70c
Alex Thompson <ahthomps1@gmail.com>
2021-04-20 14:02:32 -0600

    Add test api data into Apps state from PostMan

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -5,7 +5,7 @@ const Story = () => {
 
   return (
     <article>
-      <img />
+      <img src='https://static01.nyt.com/images/2021/04/19/climate/19CLI-BIDENCLIMATE1/19CLI-BIDENCLIMATE1-superJumbo.jpg' />
       <h2>Title</h2>
       <p>Author</p>
 
