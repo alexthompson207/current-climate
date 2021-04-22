386edf0b426ee7d3f2993f77233543918f450db5
Alex Thompson <ahthomps1@gmail.com>
2021-04-22 14:36:50 -0600

    Add a button to go back from single story view

--- src/components/StoryDetails/StoryDetails.js
+++ src/components/StoryDetails/StoryDetails.js
@@ -1,18 +1,22 @@
 import './storyDetails.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StoryDetails = ({ currentStory }) => {
   console.log(currentStory)
   const { title, id, photo, photoAlt, publishedDate, author, overview, link } = currentStory;
 
   return (
-    <section className='story-details-view'>
-      <img className='details-img' src={photo} alt={photoAlt}></img>
-      <h1 className='details-title'>{title}</h1>
-      <p className='details-author'>{author}</p>
-      <h2 className='details-overview'>{overview}</h2>
-      <a href={link} className='details-link' target='_blank'>View the Article on the NYT</a>
-    </section>
+    <>
+      <Link to='/' className='back-button'>Home</Link>
+      <section className='story-details-view'>
+        <img className='details-img' src={photo} alt={photoAlt}></img>
+        <h1 className='details-title'>{title}</h1>
+        <p className='details-author'>{author}</p>
+        <h2 className='details-overview'>{overview}</h2>
+        <a href={link} className='details-link' target='_blank'>View the Article on the NYT</a>
+      </section>
+    </>
   )
 }
 
