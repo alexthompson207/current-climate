8c3089f807c92447deac728b36d7960bf05d373b
Alex Thompson <ahthomps1@gmail.com>
2021-04-21 18:08:59 -0600

    Remove old cleaning function and api call from App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,25 +5,6 @@ import React, { Component } from 'react';
 import NewsView from '../NewsView/NewsView';
 import Header from '../Header/Header';
 
-// const generateId = (index) => {
-//   return index + new Date().getTime();
-// }
-
-// const cleanStoriesData = (stories) => {
-//   return stories.results.map((story, i) => {
-//     return {
-//       id: generateId(i),
-//       title: story.title,
-//       author: story.byline,
-//       publishedDate: story.published_date,
-//       overview: story.abstract,
-//       link: story.url,
-//       photo: story.multimedia[0].url,
-//       photoAlt: story.multimedia[0].caption,
-//     }
-//   })
-// }
-
 class App extends Component {
   constructor() {
     super()
