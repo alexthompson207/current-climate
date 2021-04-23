54ddda6643d834aa954797d616c03fc2ba0d7558
Alex Thompson <ahthomps1@gmail.com>
2021-04-23 16:37:17 -0600

    Add styling to results header element

--- src/components/NewsView/NewsView.js
+++ src/components/NewsView/NewsView.js
@@ -28,7 +28,7 @@ const NewsView = ({ stories, filteredStories, searching }) => {
 
   return (
     <section className='news-view'>
-      <h2>Number of articles ({storyCards.length})</h2>
+      <h2 className='news-results'>Number of articles: ({storyCards.length})</h2>
       {storyCards}
     </section>
   )
