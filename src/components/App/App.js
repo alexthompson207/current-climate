817c5b53c76d1ae3446697f0232be801eb98e47b
Alex Thompson <ahthomps1@gmail.com>
2021-04-24 17:03:27 -0600

    Add functionality to click on favorite button and have it change color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,7 @@ class App extends Component {
       stories: [],
       filteredStories: [],
       searching: false,
+      favorites: [],
       error: ''
     }
   }
--- src/components/StoryDetails/StoryDetails.js
+++ src/components/StoryDetails/StoryDetails.js
@@ -1,15 +1,31 @@
 import './storyDetails.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const StoryDetails = ({ currentStory }) => {
-  console.log(currentStory)
+  const [isClicked, setisClicked] = useState(false)
   const { title, id, photo, photoAlt, publishedDate, author, overview, link } = currentStory;
+
+  const handleClick = () => {
+    console.log('clicked')
+    setisClicked(!isClicked);
+    favoriteButton()
+  }
+
+  const favoriteButton = () => {
+    if (!isClicked) {
+      return '/images/heart.svg'
+    } else {
+      return '/images/red-heart.svg'
+    }
+  }
+
 
   return (
     <>
       <Link to='/' className='back-button'>Home</Link>
       <section className='story-details-view'>
+        <button onClick={() => handleClick()} className='fav-btn'><img className='fav-icon' src={favoriteButton()} alt='favorite heart icon' /></button>
         <h1 className='details-title'>{title}</h1>
         <img className='details-img' src={photo} alt={photoAlt}></img>
         <h2 className='details-overview'>{overview}</h2>
