91bc9621fa143c1b14c4c45f06692cb8734fd68e
Alex Thompson <ahthomps1@gmail.com>
2021-04-24 12:45:55 -0600

    Add client side error tests

--- cypress/integration/examples/mainPage.js
+++ cypress/integration/examples/mainPage.js
@@ -1,4 +1,4 @@
-describe('Home View Testing', () => {
+describe('Home View Features', () => {
 
   beforeEach(() => {
     cy.fixture('testStories').then(stories => {
@@ -43,5 +43,97 @@ describe('Home View Testing', () => {
   it('should display a search bar', () => {
     cy.get('.search-label').contains('Search by Title');
     cy.get('input[name="search"]').should('have.class', 'search-input');
-  })
+  });
+
+  it('should be able to click on an article and the url will update', () => {
+    cy.get('.story-card').first().click()
+    cy.url().should('include', '2021-04-23T17:56:13-04:00')
+  });
+
+  it('should be able to click on a different article and the url will update', () => {
+    cy.get('.story-card').eq(1).click()
+    cy.url().should('include', '2021-04-22T06:00:01-04:00')
+  });
+
+  it('should display article article title after clicking on an article', () => {
+    cy.get('.story-card').eq(1).click()
+    cy.get('h1').contains('The U.S. Has a New Climate Goal. How Does It Stack Up Globally?')
+  });
+
+})
+
+describe('Home View Search', () => {
+
+  beforeEach(() => {
+    cy.fixture('testStories').then(stories => {
+      cy.intercept('https://api.nytimes.com/svc/topstories/v2/climate.json?api-key=b3M1MC9DPZ6AYoCBVQ98cGQZYXRjwuoZ', stories)
+    })
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should allow users to select the search input and type an article title', () => {
+    cy.get('input[name="search"]')
+      .type('Globally')
+      .should('have.value', 'Globally')
+  });
+
+  it('should only display articles whose titles match the search input ', () => {
+    cy.get('.news-results').contains('3');
+    cy.get('.news-view a').should('have.length', 3)
+
+    cy.get('input[name="search"]')
+      .type('Globally')
+      .should('have.value', 'Globally')
+
+    cy.get('.news-results').contains('1');
+    cy.get('.news-view a').should('have.length', 1)
+    cy.get('.news-view').contains('The U.S. Has a New Climate Goal. How Does It Stack Up Globally?');
+  });
+
+  it('should display an error message if no articles match search criteria', () => {
+    cy.get('input[name="search"]')
+      .type('Nothing')
+
+    cy.get('.news-view a').should('have.length', 0)
+    cy.get('h1').contains('No articles match your search, please try again!')
+  });
+
+  it('should be able to clear search and see all articles', () => {
+    cy.get('input[name="search"]')
+      .type('Nothing')
+
+    cy.get('.news-view a').should('have.length', 0)
+    cy.get('input[name="search"]').clear()
+    cy.get('.news-view a').should('have.length', 3)
+  });
+})
+
+describe('Home View Client-Side Error', () => {
+
+  beforeEach(() => {
+    cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/climate.json?api-key=b3M1MC9DPZ6AYoCBVQ98cGQZYXRjwuoZ', {
+      statusCode: 404
+    })
+
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display an error message when a data request is broken ', () => {
+    cy.get('.error-view').contains('Oops, something went wrong')
+  });
+})
+
+describe.only('Home View Client-Side Error', () => {
+
+  beforeEach(() => {
+    cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/climate.json?api-key=b3M1MC9DPZ6AYoCBVQ98cGQZYXRjwuoZ', {
+      statusCode: 404
+    })
+
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display an error message when a data request is broken ', () => {
+    cy.get('.error-view').contains('Oops, something went wrong')
+  });
 })
