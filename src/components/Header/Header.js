dc05ebf823d822d5d302330b474a2458e013c0fe
Alex Thompson <ahthomps1@gmail.com>
2021-04-24 18:10:18 -0600

    Style header componet and button to favorites view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
           <img className='nav-logo' src='/images/logo2.svg' alt='website logo' />
         </Link>
         <Link to='/articles/favorites'>
-          <button>Favorties</button>
+          <button className='nav-link-fav'>Favorties</button>
         </Link>
       </div>
     </header>
